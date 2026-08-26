586919f2f9515673f66937f638a1386c9cf09502
AntonioKZ <36471671+AntonioKZ@users.noreply.github.com>
2026-08-26 18:18:30 +0200

    feat: route survey campaign manager

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import Documents from './Documents.tsx'
 import SurveyCenter from './SurveyCenter.tsx'
 import SurveyLinks from './SurveyLinks.tsx'
 import SurveyInbox from './SurveyInbox.tsx'
+import SurveyCoverage from './SurveyCoverage.tsx'
+import SurveyCampaign from './SurveyCampaign.tsx'
 import DataVault from './DataVault.tsx'
 import ExecutiveStatus from './ExecutiveStatus.tsx'
 import PublicSurvey from './PublicSurvey.tsx'
@@ -18,9 +20,8 @@ import Governance from './Governance.tsx'
 import ControlCenter from './ControlCenter.tsx'
 import ReadinessGate from './ReadinessGate.tsx'
 import EmployeeInsights from './EmployeeInsights.tsx'
-import SurveyCoverage from './SurveyCoverage.tsx'
 import CloudSync from './CloudSync.tsx'
 const path=window.location.pathname
-const page=path.startsWith('/survey/')?<PublicSurvey/>:path.startsWith('/survey-links')?<SurveyLinks/>:path.startsWith('/survey-inbox')?<SurveyInbox/>:path.startsWith('/survey-coverage')?<SurveyCoverage/>:path.startsWith('/employee-insights')?<EmployeeInsights/>:path.startsWith('/documents')?<Documents/>:path.startsWith('/surveys')?<SurveyCenter/>:path.startsWith('/data-vault')?<DataVault/>:path.startsWith('/executive-status')?<ExecutiveStatus/>:path.startsWith('/executive-cockpit')?<ExecutiveCockpit/>:path.startsWith('/benefits')?<BenefitsTracker/>:path.startsWith('/governance')?<Governance/>:path.startsWith('/control-center')?<ControlCenter/>:path.startsWith('/readiness')?<ReadinessGate/>:path.startsWith('/opportunity-engine')?<OpportunityEngine/>:path.startsWith('/business-case')?<BusinessCaseLab/>:<><App/><a className="vault-fab" href="/data-vault">Data Vault</a><a className="survey-fab" href="/surveys">Questionari & Assessment</a><a className="docs-fab" href="/documents">Documenti & Knowledge</a><a className="status-fab" href="/control-center">Program Control Center</a></>
+const page=path.startsWith('/survey/')?<PublicSurvey/>:path.startsWith('/survey-links')?<SurveyLinks/>:path.startsWith('/survey-campaign')?<SurveyCampaign/>:path.startsWith('/survey-inbox')?<SurveyInbox/>:path.startsWith('/survey-coverage')?<SurveyCoverage/>:path.startsWith('/employee-insights')?<EmployeeInsights/>:path.startsWith('/documents')?<Documents/>:path.startsWith('/surveys')?<SurveyCenter/>:path.startsWith('/data-vault')?<DataVault/>:path.startsWith('/executive-status')?<ExecutiveStatus/>:path.startsWith('/executive-cockpit')?<ExecutiveCockpit/>:path.startsWith('/benefits')?<BenefitsTracker/>:path.startsWith('/governance')?<Governance/>:path.startsWith('/control-center')?<ControlCenter/>:path.startsWith('/readiness')?<ReadinessGate/>:path.startsWith('/opportunity-engine')?<OpportunityEngine/>:path.startsWith('/business-case')?<BusinessCaseLab/>:<><App/><a className="vault-fab" href="/data-vault">Data Vault</a><a className="survey-fab" href="/surveys">Questionari & Assessment</a><a className="docs-fab" href="/documents">Documenti & Knowledge</a><a className="status-fab" href="/control-center">Program Control Center</a></>
 const managerPage=!path.startsWith('/survey/')
 createRoot(document.getElementById('root')!).render(<StrictMode>{page}{managerPage&&<CloudSync/>}</StrictMode>)
