df8c0c320c04eaf9015882933b3846de5b688dda
AntonioKZ <36471671+AntonioKZ@users.noreply.github.com>
2026-08-26 16:25:46 +0200

    route: add survey response inbox

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx'
 import Documents from './Documents.tsx'
 import SurveyCenter from './SurveyCenter.tsx'
 import SurveyLinks from './SurveyLinks.tsx'
+import SurveyInbox from './SurveyInbox.tsx'
 import DataVault from './DataVault.tsx'
 import ExecutiveStatus from './ExecutiveStatus.tsx'
 import PublicSurvey from './PublicSurvey.tsx'
@@ -15,15 +16,17 @@ const content=path.startsWith('/survey/')
   ? <PublicSurvey/>
   : path.startsWith('/survey-links')
     ? <SurveyLinks/>
-    : path.startsWith('/documents')
-      ? <Documents/>
-      : path.startsWith('/surveys')
-        ? <SurveyCenter/>
-        : path.startsWith('/data-vault')
-          ? <DataVault/>
-          : path.startsWith('/executive-status')
-            ? <ExecutiveStatus/>
-            : <><App/><a className="vault-fab" href="/data-vault">Data Vault</a><a className="survey-fab" href="/surveys">Questionari & Assessment</a><a className="docs-fab" href="/documents">Documenti & Knowledge</a><a className="status-fab" href="/executive-status">Executive Status</a></>
+    : path.startsWith('/survey-inbox')
+      ? <SurveyInbox/>
+      : path.startsWith('/documents')
+        ? <Documents/>
+        : path.startsWith('/surveys')
+          ? <SurveyCenter/>
+          : path.startsWith('/data-vault')
+            ? <DataVault/>
+            : path.startsWith('/executive-status')
+              ? <ExecutiveStatus/>
+              : <><App/><a className="vault-fab" href="/data-vault">Data Vault</a><a className="survey-fab" href="/surveys">Questionari & Assessment</a><a className="docs-fab" href="/documents">Documenti & Knowledge</a><a className="status-fab" href="/executive-status">Executive Status</a></>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>{content}</StrictMode>,
