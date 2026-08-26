12e01417558e5229fee79c85c438567e981ec69b
AntonioKZ <36471671+AntonioKZ@users.noreply.github.com>
2026-08-26 15:40:09 +0200

    Route Document Intelligence workspace

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './calendar.css'
 import App from './App.tsx'
+import Documents from './Documents.tsx'
+
+const path=window.location.pathname
+const content=path.startsWith('/documents')?<Documents/>:<><App/><a className="docs-fab" href="/documents">Documenti & Knowledge</a></>
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode>{content}</StrictMode>,
 )
