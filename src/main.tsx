a69c4084a68a80f504f664f64e84c223a280def4
AntonioKZ <36471671+AntonioKZ@users.noreply.github.com>
2026-08-26 16:09:16 +0200

    route: add Survey Control Center workspace

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import './index.css'
 import './calendar.css'
 import App from './App.tsx'
 import Documents from './Documents.tsx'
+import SurveyCenter from './SurveyCenter.tsx'
 
 const path=window.location.pathname
-const content=path.startsWith('/documents')?<Documents/>:<><App/><a className="docs-fab" href="/documents">Documenti & Knowledge</a></>
+const content=path.startsWith('/documents')
+  ? <Documents/>
+  : path.startsWith('/surveys')
+    ? <SurveyCenter/>
+    : <><App/><a className="survey-fab" href="/surveys">Questionari & Assessment</a><a className="docs-fab" href="/documents">Documenti & Knowledge</a></>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>{content}</StrictMode>,
