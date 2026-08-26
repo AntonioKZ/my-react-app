93655c3dedb453e2f177786fbf564aaa5a11fd4b
AntonioKZ <36471671+AntonioKZ@users.noreply.github.com>
2026-08-26 16:11:05 +0200

    route: add Data Vault workspace

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import './calendar.css'
 import App from './App.tsx'
 import Documents from './Documents.tsx'
 import SurveyCenter from './SurveyCenter.tsx'
+import DataVault from './DataVault.tsx'
 
 const path=window.location.pathname
 const content=path.startsWith('/documents')
   ? <Documents/>
   : path.startsWith('/surveys')
     ? <SurveyCenter/>
-    : <><App/><a className="survey-fab" href="/surveys">Questionari & Assessment</a><a className="docs-fab" href="/documents">Documenti & Knowledge</a></>
+    : path.startsWith('/data-vault')
+      ? <DataVault/>
+      : <><App/><a className="vault-fab" href="/data-vault">Data Vault</a><a className="survey-fab" href="/surveys">Questionari & Assessment</a><a className="docs-fab" href="/documents">Documenti & Knowledge</a></>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>{content}</StrictMode>,
